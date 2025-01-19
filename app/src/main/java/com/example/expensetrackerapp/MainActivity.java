6fcebcf241255bff9b308781f64adaf2d51672f9
Ariel Fainshtein <ariel@Ariels-iMac.local>
2025-01-19 13:32:26 +0200

    skeleton for main activity

--- app/src/main/java/com/example/expensetrackerapp/MainActivity.java
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.expensetrackerapp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        // Set default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        // Set up the listener
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_settings) {
+                selectedFragment = new SettingsFragment();
+            } else if (item.getItemId() == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (item.getItemId() == R.id.nav_statistics) {
+                selectedFragment = new StatisticsFragment();
+            } else if (item.getItemId() == R.id.nav_categories) {
+                selectedFragment = new CategoriesFragment();
+            }
+
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
+
     }
 }
